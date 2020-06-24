74c857cef558b3b08c7df683ac457c8f15ccd2d7
Leszek Pietrzak <leszek@magicznyleszek.xyz>
2020-06-24 14:19:06 +0200

    run game after 1s, intro card tweaks
    part of #24

--- source/index.ts
+++ source/index.ts
@@ -1,6 +1,8 @@
 import { Existence } from "./existence";
 
 window.onload = (): void => {
-  const humanExistence = new Existence(1000000, true);
-  humanExistence.startLife();
+  window.setTimeout(() => {
+    const humanExistence = new Existence(1000000, true);
+    humanExistence.startLife();
+  }, 1000);
 };
